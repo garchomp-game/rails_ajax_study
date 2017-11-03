3a5315aaa4b76549c987956319c8632250c994b6
garchomp-game <otoka.japanesedrum@gmail.com>
2017-11-04 02:14:01 +0900

    ajaxフォーム実装１：途中（エラー起きてるところでストップ）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/'=>'practice#index'
   post '/'=>'practice#index'
+  resources :apples
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
